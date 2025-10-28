8b1765ce27d7545d5e41ddba6e50c9ca6d26fe82
Aslah <168297709+aslahkp01@users.noreply.github.com>
2025-10-28 22:10:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.67%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,9 +5,9 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        k = 0  # pointer for placing elements that are NOT equal to val
+        k=0
         for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]  # move non-val element to the front
-                k += 1
+            if nums[i]!=val :
+                nums[k]=nums[i]
+                k+=1
         return k
