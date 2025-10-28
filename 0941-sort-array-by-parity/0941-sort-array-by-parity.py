df0e0871834fbf40b73a51a5061eb9ebcca77775
Aslah <168297709+aslahkp01@users.noreply.github.com>
2025-10-28 20:58:22 +0530

    Time: 0 ms (100%), Space: 12.9 MB (88.02%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left=0
+        right=len(nums)-1
+        while left < right:
+           if nums[left]%2>nums[right]%2:
+            nums[left],nums[right]=nums[right],nums[left]
+
+           if nums[left]%2==0 :
+                left+=1
+           if nums[right]%2==1 :
+                right-=1
+        return nums
