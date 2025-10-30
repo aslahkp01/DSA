0cbfa5a26c711e21717a2eb93bac12655344df9d
Aslah <168297709+aslahkp01@users.noreply.github.com>
2025-10-30 19:31:14 +0530

    Time: 0 ms (100%), Space: 18 MB (11.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,25 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0 = count1 = count2 = 0
-
-        # Count occurrences
-        for num in nums:
-            if num == 0:
-                count0 += 1
-            elif num == 1:
-                count1 += 1
-            else:
-                count2 += 1
-
-        # Rebuild array using counts
-        index = 0
-        for _ in range(count0):
-            nums[index] = 0
-            index += 1
-        for _ in range(count1):
-            nums[index] = 1
-            index += 1
-        for _ in range(count2):
-            nums[index] = 2
-            index += 1
+        low=0
+        mid=0
+        high=len(nums)-1
+        while mid<=high :
+            if nums[mid]==0 :
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1 :
+                mid+=1
+            else :
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
